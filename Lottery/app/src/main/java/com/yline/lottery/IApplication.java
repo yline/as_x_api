b8177752673e64da9f329eed0798dbd621b5fc10
ylineWork <linjiang@kjtpay.local>
2018-08-24 17:59:25 +0800

    this:集成 bugly

--- Lottery/app/src/main/java/com/yline/lottery/IApplication.java
+++ Lottery/app/src/main/java/com/yline/lottery/IApplication.java
@@ -1,17 +1,32 @@
 package com.yline.lottery;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
 
 import com.yline.application.SDKManager;
 import com.yline.http.OkHttpUtils;
+import com.yline.lottery.bugly.BuglyConfig;
 
 public class IApplication extends Application {
+	
+	@Override
+	protected void attachBaseContext(Context base) {
+		super.attachBaseContext(base);
+		MultiDex.install(this);
+	}
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		
+		// Http
 		OkHttpUtils.init(true);
+		
+		// LibSDK
 		SDKManager.init(this, null);
+		
+		// bugly
+		BuglyConfig.init(this, true);
 	}
 }
